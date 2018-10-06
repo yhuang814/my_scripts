b84dfb9a9b1b312a05c336be7536a2ad7c92427b
Yuliang Huang <yuliang.huang@borngroup.com>
2018-10-05 21:28:33 -0400

    update to better group tickets and print out failed tickets

--- commits_report/commit_version.py
+++ commits_report/commit_version.py
@@ -20,6 +20,12 @@ def get_repo_dir() :
         return config['repo_dir']
     return
 
+def get_base_url() :
+    config = get_config()
+    if config['jira']['base_url'] : 
+        return config['jira']['base_url']
+    return
+
 def get_ticket_versions(ticket_id) : 
     config = get_config()
     jira_config = config['jira']
@@ -30,8 +36,12 @@ def get_ticket_versions(ticket_id) :
 
     if(response.status_code == 200) :
         response_obj = response.json()
-        return response_obj['fields']['fixVersions']
-    return
+        response_fix_versions = response_obj['fields']['fixVersions']
+        fix_versions = []
+        for version in response_fix_versions:
+            fix_versions.append(version['name'])
+        return fix_versions
+    return []
 
 #get JIRA ticket number from the commit message using regular expression
 def get_ticket_num(commit_message) : 
@@ -42,7 +52,33 @@ def get_ticket_num(commit_message) :
         return matches.groups()
     
     return
+
+def get_grouped_tickets(commits):
+    ticket_list = {}
+
+    #group commits by ticket #
+    for commit in commits : 
+        commit_author = commit['author']['name']
+        commit_id = commit['commit']
+        commit_message = commit['message']
+
+        tickets = get_ticket_num(commit_message)
+        if tickets : 
+            for ticket in tickets : 
+                if ticket is not None : 
+                    if ticket not in ticket_list :
+                        ticket_list[ticket] = {
+                            "authors" : [],
+                            "commits" : []
+                        }
+                    if commit_author not in ticket_list[ticket]["authors"]:
+                        ticket_list[ticket]["authors"].append(commit_author)
+                    
+                    if commit_id not in ticket_list[ticket]["commits"]:
+                        ticket_list[ticket]["commits"].append(commit_id)
         
+    return ticket_list
+
 
 def main():
     os_command = []
@@ -51,22 +87,32 @@ def main():
     repo_dir = get_repo_dir() #--git-dir
     if repo_dir : 
         os_command.append("--git-dir=" + repo_dir + '/.git')
-    os_command.append("--compare=origin/master..origin/hotfix_0.17.2")
+    os_command.append("--compare=0.16.9..0.17")
     os_command = " ".join(os_command)
     
     os_output = os.popen(os_command).read()
     commits = json.loads(os_output);
 
-    parsed_commits = []
+    ticket_list = get_grouped_tickets(commits)
     
-    for commit in commits : 
-        d = {
-            "author" : commit['author']['name'],
-            "commit" : commit['commit'],
-            "message" : commit['message']
-        }
-        d["tickets"] = get_ticket_num(d['message'])
-        parsed_commits.append(d)
+    check_version = "0.17"
+    base_url = get_base_url()
+
+    for ticket in ticket_list: 
+        fix_versions = get_ticket_versions(ticket)
+        if check_version in fix_versions :
+            result = {
+                "success" : True,
+                "message" : "Passed"
+            }
+        else : 
+            result = {
+                "success" : False,
+                "message" : "Failed"
+            }
+        if result["success"] is False : 
+            ticket_url = base_url + "/browse/" + ticket
+            print("ticket #: " + ticket + " - " + " ".join(fix_versions) + " - " + result["message"] + " - " + ticket_url)
 
 if __name__ == '__main__':
     	main()
